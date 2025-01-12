cabc845814949e9ab7dfe807b75a1665adf9b589
Ahsan <sajibnet@gmail.com>
2025-01-12 02:16:59 +0200

    Redux Excercise 6.14 to 6.15

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,10 +1,20 @@
 //redux-anecdotes/src/App.jsx
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdotesService from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdotesService.getAll().then(anecdotes =>dispatch(setAnecdotes(anecdotes)))
+})
+  
+  
   return (
     <div>
       <h2>Anecdotes</h2>
--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,17 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { voteAnecdote } from '../reducers/anecdoteReducer';
 import { setNotification,removeNotification } from '../reducers/notificationReducer';
+import anecdoteService from '../services/anecdotes';  
 
 const AnecdoteList = () => {
   const dispatch = useDispatch();
-  const anecdotes = useSelector(({ anecdotes, filter }) => //getting filter state from store and filtering anecdotes based on the filter
-    [...anecdotes]
-      .filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase())) // filter state is a string and we are filtering the anecdotes based on the filter
-  );
 
-  const vote = (id, content) => {
-    dispatch(voteAnecdote(id));
-    dispatch(setNotification(`You voted '${content}'`));
+  const anecdotes = useSelector(({ anecdotes, filter }) => {//getting filter state from store and filtering anecdotes based on the filter
+    // [...anecdotes]
+    //   .filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase())) // filter state is a string and we are filtering the anecdotes based on the filter
+    //   .sort((a, b) => b.votes - a.votes) // Sort the anecdotes based on the number of votes
+    return anecdotes
+      .filter((anecdote) => 
+        anecdote.content && filter ? 
+        anecdote.content.toLowerCase().includes(filter.toLowerCase()) : 
+        true
+      )
+      .sort((a, b) => b.votes - a.votes);
+});
+
+  const vote = async (id) => {
+    const anectodeToVote = anecdotes.find((anecdote) => anecdote.id === id);
+    const updatedAnecdote = { ...anectodeToVote, votes: anectodeToVote.votes + 1 };
+    await anecdoteService.updateVote(id,updatedAnecdote);//update the votes in the server
+    
+    dispatch(voteAnecdote(updatedAnecdote));//update the votes in the store
+
+    dispatch(setNotification(`You voted '${anectodeToVote.content}'`));
     setTimeout(() => {
       dispatch(removeNotification());
     }, 5000); // Remove the notification after 5 seconds
@@ -23,8 +38,7 @@ const AnecdoteList = () => {
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
+            has : {anecdote.votes} <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
       ))}
